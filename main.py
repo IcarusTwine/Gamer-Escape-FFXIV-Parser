9a18c28865964b0d9fe2cb42965cb6f3cf113ed5
Icarus <jon.sollis@hotmail.co.uk>
2024-11-22 16:20:20 +0000

    init

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 from Utils.github import sync_github_folder
 
-# Example usage:
-owner = "xivapi"
-repo = "ffxiv-datamining"
-folder_path = "csv"
-local_path = "./csv"
-branch = "master"
-token = None  # Add your personal access token if needed for private repositories
-
-sync_github_folder(owner, repo, folder_path, local_path, branch, token)
+sync_github_folder("xivapi", "ffxiv-datamining", "csv", "./csv", "master", None)
