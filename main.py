5df9b18172299686882aff01d88676d22e574f97
Icarus <jon.sollis@hotmail.co.uk>
2024-11-22 16:40:09 +0000

    fix

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from Utils.github import sync_github_folder
 
 sync_github_folder("xivapi", "ffxiv-datamining", "csv", "./csv", "master", None)
+sync_github_folder("xivapi", "ffxiv-datamining-patches", "patchdata", "./patchdata", "master", None)
